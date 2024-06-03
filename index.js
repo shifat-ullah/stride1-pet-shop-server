0ee64d2c65fc2aa1db3e2136b3e1f85e91d65023
unknown <shifatullah128@gmail.com>
2024-06-04 00:04:40 +0600

    successfully conected mongodb

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
+const cors= require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors())
+//pet-shop
+// e9v17hefgi1dlsJC
+
+
+
+
+const uri = "mongodb+srv://pet-shop:e9v17hefgi1dlsJC@cluster0.nxcosv7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const petCollection = client.db('petShop').collection('pets')
+
+// 
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
